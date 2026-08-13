126399d9c6c50759f2e288503495d0d6ce358077
Batihan Eyyubi <batocode@gmail.com>
2026-08-13 09:23:00 +0300

    up

--- westdesk-frontend/src/api/client.ts
+++ westdesk-frontend/src/api/client.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-
-const API_BASE_URL = "http://localhost:5004/api";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
 });
-
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
