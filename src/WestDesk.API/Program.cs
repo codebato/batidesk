9f430b751a9e6d2bdec7a911176cbbdfa76209e8
Batihan Eyyubi <batocode@gmail.com>
2026-08-14 17:28:17 +0300

    up

--- src/WestDesk.API/Program.cs
+++ src/WestDesk.API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
-                "https://batidesk-frontend.onrender.com"
+                "https://westdesk-frontend.onrender.com"
               )
                       .AllowAnyHeader()
                       .AllowAnyMethod()
