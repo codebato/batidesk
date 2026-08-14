0d39b53b3bfdb02d11ab1fe2ede503ed3efa92c6
Batihan Eyyubi <batocode@gmail.com>
2026-08-14 17:24:39 +0300

    up

--- src/WestDesk.API/Program.cs
+++ src/WestDesk.API/Program.cs
@@ -73,8 +73,22 @@ builder.Services
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
 
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
 
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+            {
+                context.Token = accessToken;
+            }
+
+            return Task.CompletedTask;
+        }
     };
 });
 
